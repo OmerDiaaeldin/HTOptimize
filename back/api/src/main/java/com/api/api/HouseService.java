6706cc7833e9b4792a5afd6a2ce092eb8ef9b411
OmerDiaaeldin <omardiaaeldin2592001@gmail.com>
2024-11-21 16:05:10 +0100

    restrucure again

---
+++ back/api/src/main/java/com/api/api/HouseService.java
@@ -0,0 +1,31 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HouseService {
+    @Autowired
+    private HouseRepository houseRepository;
+
+    // Create or Save a House
+    public House saveHouse(House house) {
+        return houseRepository.save(house);
+    }
+
+    // Retrieve All Houses
+    public List<House> getAllHouses() {
+        return houseRepository.findAll();
+    }
+
+    // Retrieve a House by ID
+    public Optional<House> getHouseById(String id) {
+        return houseRepository.findById(id);
+    }
+
+    // Delete a House by ID
+    public void deleteHouseById(String id) {
+        houseRepository.deleteById(id);
+    }
+}
